21694eaab39499b56a0278e62de042747eeb081a
IamChplayz <165848128+IamChplayz@users.noreply.github.com>
2026-03-17 14:45:50 -0400

    Add files via upload

---
+++ Core_Files/Rgu_Pen_Backup_Uninstaller.bat
@@ -0,0 +1,11 @@
+@echo off
+del /f /q "%AppData%\RoGold Ultimate\extensions\rogold-ultimate\manifest.json"
+
+echo.
+echo ===========================================
+echo RGU Pen has been uninstalled. restart the rgu app for it to take effect.
+echo ===========================================
+echo.
+
+pause
+exit
